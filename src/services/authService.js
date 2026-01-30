de3f128ae629f2734373dafef68e4217df86c1ca
Nguyễn Trường Huy <huyhuyhuy201107@gmail.com>
2026-01-30 20:25:37 +0700

    merge khoahnd

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
+const API_BASE_URL ="http://localhost:8080";
 
 export async function login({ email, password }) {
   const response = await axios.post(`${API_BASE_URL}/api/auth/login`, {
