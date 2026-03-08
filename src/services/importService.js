2171230ea672a8f029d51c9bb9f3387951ff9a02
KhangThinh0331 <khangthinh0331@gmail.com>
2026-03-08 20:09:35 +0700

    update service files

--- src/services/importService.js
+++ src/services/importService.js
@@ -1,7 +1,7 @@
-// const API_URL = "http://localhost:8080/api/imports";
-// const BASE_URL = "http://localhost:8080";
-const API_URL = "https://ec-website-be-312564370609.asia-southeast1.run.app/api/imports";
-const BASE_URL = "https://ec-website-be-312564370609.asia-southeast1.run.app";
+const API_URL = "http://localhost:8080/api/imports";
+const BASE_URL = "http://localhost:8080";
+// const API_URL = "https://ec-website-be-312564370609.asia-southeast1.run.app/api/imports";
+// const BASE_URL = "https://ec-website-be-312564370609.asia-southeast1.run.app";
 
 // Helper to get auth token
 function getAuthHeaders() {
