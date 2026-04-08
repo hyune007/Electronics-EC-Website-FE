c3586aa7fc46b21ead9e69c116611cf9bc219307
kohacoder <172766095+kohacoder@users.noreply.github.com>
2026-04-08 22:13:07 +0700

    pull quanvl-fe

--- src/components/customer/product/OrderDetailModalCustomer.jsx
+++ src/components/customer/product/OrderDetailModalCustomer.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import { createPortal } from "react-dom";
 import { getBillDetails } from "../../../services/customer/billDetailServiceCustomer";
 import { formatVND } from "../../../utils/priceFormatter";
